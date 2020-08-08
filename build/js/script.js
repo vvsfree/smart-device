7564ad2126d33e38ea4d3b4a87be9637587ca398
Vyacheslav Soldatov <vvsfree@yandex.ru>
2020-08-08 20:59:44 +0600

    Добавляет главный экран (блок промо), графику

--- build/js/script.js
+++ build/js/script.js
@@ -1,12 +1,13 @@
 'use strict';
-
-var accordionNodeList = document.querySelectorAll('.accordion');
-if (accordionNodeList) {
-  var accordionArray = Array.prototype.slice.call(accordionNodeList);
-  accordionArray.forEach(function (item) {
-    item.classList.remove('accordion--expanded');
-    item.addEventListener('click', function () {
-      item.classList.toggle('accordion--expanded');
+(function () {
+  var accordionNodeList = document.querySelectorAll('.accordion');
+  if (accordionNodeList) {
+    var accordionArray = Array.prototype.slice.call(accordionNodeList);
+    accordionArray.forEach(function (item) {
+      item.classList.remove('accordion--expanded');
+      item.addEventListener('click', function () {
+        item.classList.toggle('accordion--expanded');
+      });
     });
-  });
-}
+  }
+})();
