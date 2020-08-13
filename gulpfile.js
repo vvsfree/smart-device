9dc5158f72c1ee6897a52f4184830ba2963bb13f
Vyacheslav Soldatov <vvsfree@yandex.ru>
2020-08-13 09:19:28 +0600

    Исправляет баг #11

--- gulpfile.js
+++ gulpfile.js
@@ -15,6 +15,7 @@ var svgstore = require('gulp-svgstore');
 var posthtml = require('gulp-posthtml');
 var include = require('posthtml-include');
 var del = require('del');
+var concat = require('gulp-concat');
 
 gulp.task('css', function () {
   return gulp.src('source/sass/style.scss')
@@ -87,7 +88,7 @@ gulp.task('copy', function () {
   return gulp.src([
     'source/fonts/**/*.{woff,woff2}',
     'source/img/**',
-    'source/js/**',
+    'source/js/*.js',
     'source//*.ico'
   ], {
     base: 'source'
@@ -100,9 +101,15 @@ gulp.task('copy-js', function () {
       .pipe(gulp.dest('build/js'));
 });
 
+gulp.task('vendor-js', function () {
+  return gulp.src('source/js/vendor/*.js')
+      .pipe(concat('vendor.js'))
+      .pipe(gulp.dest('build/js'));
+});
+
 gulp.task('clean', function () {
   return del('build');
 });
 
-gulp.task('build', gulp.series('clean', 'copy', 'css', 'sprite', 'html'));
+gulp.task('build', gulp.series('clean', 'copy', 'vendor-js', 'css', 'sprite', 'html'));
 gulp.task('start', gulp.series('build', 'server'));
